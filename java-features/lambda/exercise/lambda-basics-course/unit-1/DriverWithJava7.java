35a3dd787cc3b18773973976abb6953855bd1679
Saurav Singh <itzsrv@outlook.com>
2022-08-24 14:05:14 +0530

    fix: bug in comparator

--- java-features/lambda/exercise/lambda-basics-course/unit-1/DriverWithJava7.java
+++ java-features/lambda/exercise/lambda-basics-course/unit-1/DriverWithJava7.java
@@ -19,7 +19,7 @@ public class DriverWithJava7 {
         Collections.sort(people, new Comparator<Person>() {
             @Override
             public int compare(Person o1, Person o2) {
-                return o1.getLastName().compareTo(o2.toString());
+                return o1.getLastName().compareTo(o2.getLastName());
             }
         });
         
